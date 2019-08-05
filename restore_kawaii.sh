f7217e9b8be74948d08221dd2853b790dc5c9d8e
jigglycrumb <1476865+jigglycrumb@users.noreply.github.com>
2019-08-05 17:59:44 +0200

    Clockwork OS 0.4 compatibility (#1)
    * move all skin files to kawaii subfolder
    
    * update gitignore
    
    * first version running in 0.4
    
    * update README
    
    * update gitignore
    
    * adds install scripts
    
    * remove non-standard icons
    
    * fix typo in installer

--- restore_kawaii.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm -rf original
-mv default original
-ln -s kawaii default
---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd /home/cpi
+if [ ! -d mods ]; then
+  mkdir mods
+fi
+
+cd mods
+if [ ! -d gameshell-skin-kawaii ]; then
+  git clone https://github.com/hpcodecraft/gameshell-skin-kawaii.git
+fi
+
+cd gameshell-skin-kawaii
+git pull
+./install.sh
+
+cd /home/cpi
