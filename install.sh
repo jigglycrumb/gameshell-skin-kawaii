94d0c0f9397d11076dcc0405962fe9380eb17111
jigglycrumb <1476865+jigglycrumb@users.noreply.github.com>
2019-08-06 14:42:25 +0200

    adds keymap for SNES

--- install.sh
+++ install.sh
@@ -8,6 +8,21 @@ HERE=$(pwd)
 
 printf "${GREEN}Installing skin...${NC}\n"
 
-cp -R "${HERE}/kawaii" ${SKINS_DIR}
+cp -R "${HERE}/kawaii" "${SKINS_DIR}"
+
+printf "${GREEN}Please choose your keymap: (x)box or (s)nes${NC}\n"
+
+read USER_INPUT
+
+cd "${SKINS_DIR}/sys.py/gameshell/footbar_icons"
+
+if [ "${USER_INPUT}" == "x" ]; then
+  ln -s footbar-xbox.png footbar.png
+elif [ "${USER_INPUT}" == "s" ]; then
+  ln -s footbar-snes.png footbar.png
+else
+  echo "Unknown option, installing xbox keymap"
+  ln -s footbar-xbox.png footbar.png
+fi
 
 printf "${GREEN}Done! You can choose the skin in the launcher settings now. ${NC}\n"
